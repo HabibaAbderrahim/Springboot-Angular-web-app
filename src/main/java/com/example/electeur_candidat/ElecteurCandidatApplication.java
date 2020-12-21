1b871eea88630d24bbe1ac702cd12c1290ed62f2
HabibaAbderrahim <heububabiba@gmail.com>
2020-12-21 11:47:22 +0100

    EnableSecurity

--- src/main/java/com/example/electeur_candidat/ElecteurCandidatApplication.java
+++ src/main/java/com/example/electeur_candidat/ElecteurCandidatApplication.java
@@ -1,34 +1,13 @@
 package com.example.electeur_candidat;
 
-import com.example.electeur_candidat.DTO.MessageResponse;
-import com.example.electeur_candidat.entities.*;
-import com.example.electeur_candidat.repositories.*;
-import com.example.electeur_candidat.services.ActivityService;
-import com.example.electeur_candidat.services.CandidatService;
-import org.apache.catalina.User;
-import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 //@SpringBootApplication(exclude = {
 //		MongoAutoConfiguration.class,
