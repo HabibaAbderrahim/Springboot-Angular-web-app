da755c2d1ace2bc6e9e499c095bd6696005d3fa5
HabibaAbderrahim <heububabiba@gmail.com>
2020-12-21 19:06:01 +0100

    Send Filter Authorization

---
+++ src/main/java/com/example/electeur_candidat/config/JwtAuthorizationFilter.java
@@ -0,0 +1,57 @@
+package com.example.electeur_candidat.config;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+@Configuration
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+    @Value("${jwt.signin.key}")
+    private String signinKey;
+    @Value("${jwt.header}")
+    private String hearder;
+    @Value("${jwt.prefix}")
+    private String prefix;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String head = request.getHeader(hearder);
+
+        if (head == null || !head.startsWith(prefix)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String token = head.replace(prefix, "");
+        Claims claims = Jwts.parser().setSigningKey(signinKey.getBytes()).parseClaimsJws(token).getBody();
+        String username = claims.getSubject();
+        List<String> roles = (List<String>) claims.get("roles");
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        roles.forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role));
+        });
+
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, null,
+                authorities);
+
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/com/example/electeur_candidat/entities/Users.java
+++ src/main/java/com/example/electeur_candidat/entities/Users.java
@@ -2,13 +2,20 @@ package com.example.electeur_candidat.entities;
 
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 
 @Data
 @Document
-public class Users extends Contact {
-
+public class Users extends Contact implements UserDetails {
+    //Objet de type UserDetail
+   //NB In case usename and passw # nzid getser b esm #
     private String username ;
     private String password ;
     private String role ;
@@ -20,5 +27,36 @@ public class Users extends Contact {
                 ", password='" + password + '\'' +
                 ", role='" + role + '\'' +
                 "} " + super.toString();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(role));
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    //mdp akther men mara faut
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    //jme3et kol 6 ybadlou mdp
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    //les comptes par defaut des
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
